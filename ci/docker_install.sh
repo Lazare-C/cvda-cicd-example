a519ed64770004ab2326d771f76334260ceba7e6
LazareC <lazarela17@gmail.com>
2021-04-30 09:27:13 +0200

    change docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,7 +8,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git wget -yqq
+apt-get install git wget zip unzip php-zip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit "https://phar.phpunit.de/phpunit-9.phar"
