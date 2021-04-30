3c29d1cc1ba9da67e457936662ed1c5f70cf754d
LazareC <lazarela17@gmail.com>
2021-04-30 09:32:02 +0200

    change docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,7 +8,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git wget zip unzip php-zip -yqq
+apt-get install git wget zip unzip libzip-dev -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit "https://phar.phpunit.de/phpunit-9.phar"
@@ -19,3 +19,5 @@ chmod +x /usr/local/bin/phpunit
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install pdo_mysql
+docker-php-ext-configure zip 
+docker-php-ext-install zip
